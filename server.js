a4c0cbddd37c8b5b2d26bd5e8c8e7e0e560ea4af
kunalsali <kunalsali09@gmail.com>
2017-03-18 14:44:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two', function (req, res){
 });
 
 app.get('/article-three', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+     res.sendFile(path.join(__dirname, 'ui', 'artcle-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
