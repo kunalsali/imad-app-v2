ebeb6517e03f129ace0a74dc0eb187d0faedee96
kunalsali <kunalsali09@gmail.com>
2017-03-22 17:02:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function (req, res) {
-    counter =  +1;
+    counter = +1;
     res.send(counter.toString());
 });
 app.get('/:articlename', function (req, res){
