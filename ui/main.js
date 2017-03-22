57947e351f8a142366e635b3e540bc629cad0eab
kunalsali <kunalsali09@gmail.com>
2017-03-22 23:59:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,11 +30,31 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
-    var names = ['name1','name2','name3','name4'];
+       
+  //create request object
+  var request = new XMLHttpRequest();
+  
+  //capture the responce and store it in variable
+  request.onreadystatechange = function () {
+  if (request.readyState === XMLHttpRequest.DONE) {
+      //take some action
+      if (request.status === 200) {
+           var names = request.responseText;
+           names = JSON.parse(names);
     var list = '';
     for (var i=0; i< names.length; i++)  {
         list += '<li>' + names[i] + '<li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+     
+      }
+   }
+     //not done yet
+  };
+  
+  //make the request
+  request.open('GET', 'http://kunalsali.imad.hasura-app.io/submit-name?name=' +name, true);
+  request.send(null);
 };
+  
