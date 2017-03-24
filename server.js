0db3c794752b0afba23f6b6f7cda7c41dc1a25fa
kunalsali <kunalsali09@gmail.com>
2017-03-24 20:20:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,9 +124,22 @@ app.get('/submit-name', function (req, res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articlename', function (req, res){
+app.get('/articles/:articlename', function (req, res){
     //articlename == article-one
     //articles [articlename]== {}content object for article-one
+    
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('article not found');
+            } else {
+                var articleData = result.rows [0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
     var articlename = req.params.articlename;
     res.send(createtemplate(articles[articlename]));
 });
