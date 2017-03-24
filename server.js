85553dcc161ccab0eeba1310847ce2712c2353d3
kunalsali <kunalsali09@gmail.com>
2017-03-24 17:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'kunalsali',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-kunalsali-72737'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
